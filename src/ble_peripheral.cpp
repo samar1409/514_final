c38ee5bf7684a80234a0029aa1927956c94ce63d
samar1409 <59666510+samar1409@users.noreply.github.com>
2025-03-12 11:33:19 -0700

    ML

--- src/ble_peripheral.cpp
+++ src/ble_peripheral.cpp
@@ -1,1 +1,26 @@
-// Fully implemented ble_peripheral.cpp
+#include "ble_peripheral.h"
+
+BLEPeripheral::BLEPeripheral(const char* name) {
+    BLEDevice::init(name);
+    pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(serviceUUID);
+    pCharacteristic = pService->createCharacteristic(
+        charUUID,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+    pService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(serviceUUID);
+    BLEDevice::startAdvertising();
+}
+
+void BLEPeripheral::send(float value) {
+    char buffer[8];
+    dtostrf(value, 6, 2, buffer);
+    pCharacteristic->setValue(buffer);
+    pCharacteristic->notify();
+}
+
+void BLEPeripheral::set_callback(void (*cb)(float)) {
+    callback = cb;
+    pCharacteristic->setCallbacks(new BLECharacteristicCallbacks());
+}

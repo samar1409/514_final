4f2dbe3c353462fb13cf30c947d2e1bad67f25cf
samar1409 <59666510+samar1409@users.noreply.github.com>
2025-03-12 11:32:43 -0700

    ML

--- src/ble_peripheral.h
+++ src/ble_peripheral.h
@@ -1,1 +1,22 @@
-// Fully implemented ble_peripheral.h
+#ifndef BLE_PERIPHERAL_H
+#define BLE_PERIPHERAL_H
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+class BLEPeripheral {
+private:
+    BLEServer *pServer;
+    BLECharacteristic *pCharacteristic;
+    std::string serviceUUID = "181A";
+    std::string charUUID = "2A6E";
+    void (*callback)(float);
+
+public:
+    BLEPeripheral(const char* name = "AirQualityMonitor");
+    void send(float value);
+    void set_callback(void (*cb)(float));
+};
+
+#endif
